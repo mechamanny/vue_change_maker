bc04dfcac52fc469fd9d9740517e166f288c29ed
mechamanny <ekaunitz@gmail.com>
2017-08-01 23:44:32 -0400

    add UI and change calculation capabilities.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,30 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  data: {
+    changeInput: '',
+    changeOutput: ''
+  },
+  watch: {
+    change: function(newChange) {
+      this.calculateChange(this.changeInput)
+    }
+  },
+  methods: {
+    calculateChange: function(newChange) {
+      let denom = [100,25,10,5,1]
+      let amounts = {dollars: 0, quarters: 25, dimes: 10, nickels: 5, pennies: 1}
+      denom.forEach(function(ele,i) {
+        denom[i] = Math.floor(newChange / ele)
+        newChange = newChange % ele
+      })
+      let i = 0
+      for (let amount in amounts) {
+        amounts[amount] = denom[i]
+        i ++
+      }
+
+      this.changeOutput = amounts
+    }
+  }
 })
